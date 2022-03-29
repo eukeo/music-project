3e70581b6b587a70a5911f2a8b04b726916be952
eukeo <sixsblooms@gmail.com>
2022-03-29 00:00:13 -0700

    widget

--- public/javascripts/soundcloud.js
+++ public/javascripts/soundcloud.js
@@ -1,6 +1,9 @@
 // 1. Be able to get the data from the input field
 // 2. Save that data in a global variable AND use let so it can be changed
 // 3. Interpolate the src tag in the widget link with the variable from input
+// Let scurl = e.target.value
+// src=`kjhlhdjhw;hwg;url=${curl}`
 
 
-let scUrl = e.target
+let scUrl = e.target.value('url');
+let scSrc = (`url=${scUrl}`);
